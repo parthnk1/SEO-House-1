5e04d43423b5d2742ad4ccaf8a8f0841661b34c1
Partha Naskar <partha.arthashastragurukul@gmail.com>
2025-10-15 16:40:23 +0000

    can you use color background only for tools surrounding sides

--- src/app/tools/[slug]/layout.tsx
+++ src/app/tools/[slug]/layout.tsx
@@ -6,14 +6,16 @@ export default function ToolLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="container mx-auto px-4 py-12">
-      <div className="grid grid-cols-1 lg:grid-cols-3 xl:grid-cols-4 gap-12">
-        <div className="lg:col-span-2 xl:col-span-3">{children}</div>
-        <div className="space-y-8">
-          <AdsensePlaceholder />
-          <AdsensePlaceholder />
+    <div className="bg-muted/40">
+        <div className="container mx-auto px-4 py-12">
+        <div className="grid grid-cols-1 lg:grid-cols-3 xl:grid-cols-4 gap-12">
+            <div className="lg:col-span-2 xl:col-span-3">{children}</div>
+            <div className="space-y-8">
+            <AdsensePlaceholder />
+            <AdsensePlaceholder />
+            </div>
         </div>
-      </div>
+        </div>
     </div>
   );
 }
